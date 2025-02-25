e1d565ad19ce965c737fa8c1d575d09f02af46f9
alecmazzoli <alecmazzoli@gmail.com>
2025-02-25 17:53:25 -0500

    fixing thumbnail font size

--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -19,11 +19,10 @@ export const GET: APIRoute = async () => {
     <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
         <div style="display: flex; flex-direction: column; gap: 0.75rem;">  
-          <p style="font-size: 48px;">Alec Mazzoli</p>
-          <p style="font-size: 38px;">Writer | Filmmaker | Software Developer</p>
+          <p style="font-size: 60px;">Alec Mazzoli</p>
+          <p style="font-size: 40px;">Writer | Filmmaker | Software Developer</p>
         </div>
         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
-          <p style="font-size: 32px">${link}</p>
         </div>
       </div>
     </div>
